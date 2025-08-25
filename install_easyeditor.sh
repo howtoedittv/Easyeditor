998657bc84c12f74998938f97be6b5d765aab6af
howtoedittv <howtoedittv@gmail.com>
2025-08-25 14:10:36 +0300

    Update install_easyeditor.sh

--- install_easyeditor.sh
+++ install_easyeditor.sh
@@ -5,8 +5,10 @@ cd ee-editor
 makepkg -si
 wget -P ~/ \
 "https://github.com/howtoedittv/Easyeditor/releases/download/1.0/ee.sh"
+chmod +x ~/ee.sh
 wget -P ~/.local/share/applications/ \
 "https://github.com/howtoedittv/Easyeditor/releases/download/1.0/ee.desktop"
+chmod +x ~/.local/share/applications/ee.desktop
 mkdir -P ~/Pictures/icons_.dekstop
 wget -P ~/Pictures/icons_.dekstop/ \
 "https://static.vecteezy.com/system/resources/previews/014/476/827/non_2x/editor-icon-simple-style-vector.jpg"
