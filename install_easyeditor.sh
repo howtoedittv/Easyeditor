096c112efbe4d19fe6758fec2af365286c6de12b
howtoedittv <howtoedittv@gmail.com>
2025-09-04 12:43:29 +0300

    Add files via upload

--- install_easyeditor.sh
+++ install_easyeditor.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-set -e
-
-sudo pacman -S --needed base-devel konsole
-
-# Clone ee-editor if not already present
-[ -d "ee-editor" ] || git clone https://aur.archlinux.org/ee-editor.git
-cd ee-editor || exit
-makepkg -si
-
-# Download script and make it executable
-wget -P ~/ "https://github.com/howtoedittv/Easyeditor/releases/download/1.0/ee.sh"
+wget -p ~/ \
+"https://github.com/howtoedittv/Easyeditor/releases/download/1.0/ee.tar"
+tar -xvf ee.tar
+wget -P ~/ \
+"https://github.com/howtoedittv/Easyeditor/releases/download/1.0/ee.sh"
 chmod +x ~/ee.sh
-
-# Download desktop file and make it executable
 wget -P ~/.local/share/applications/ \
 "https://github.com/howtoedittv/Easyeditor/releases/download/1.0/ee.desktop"
 chmod +x ~/.local/share/applications/ee.desktop
-
-# Download icon
-mkdir -p ~/Pictures/icons.desktop
-wget -P ~/Pictures/icons.desktop/ \
+mkdir -P ~/Pictures/icons_.dekstop
+wget -P ~/Pictures/icons_.dekstop/ \
 "https://static.vecteezy.com/system/resources/previews/014/476/827/non_2x/editor-icon-simple-style-vector.jpg"
